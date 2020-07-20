3ae87d9e224eebb7a66bb9fc25584732b2c7953b
Andrew <andrew.collison.ac@gmail.com>
2020-07-20 17:39:18 +1000

    Ready for lab testing
    Ready to integrate into the system hardware for lab trial

--- Software/MQTTsystem.py
+++ Software/MQTTsystem.py
@@ -8,11 +8,6 @@ def on_connect(client, userdata, flags, rc):
   print("Connected with result code "+str(rc))
   client.subscribe("topic/temp1")
 
-def on_message(client, userdata, msg):
-    data = msg.payload.decode()
-    print(data)
-    client.disconnect()
-
 def on_subscribe(client, userdata, mid, granted_qos):
     print("Subscribed: "+str(mid)+" "+str(granted_qos))
 
@@ -29,10 +24,3 @@ client.subscribe("temp1", qos=1)
 
 client.loop_forever()
     
-# client = mqtt.Client()
-# client.connect("192.168.0.100",1883,60)
-
-# client.on_connect = on_connect
-# client.on_message = on_message
-
-# client.loop_forever()
