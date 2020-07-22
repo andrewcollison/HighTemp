7d21456d32eb06b081e067b62d59b862fc8de50e
Andrew Collison <andrew.collison.ac@gmail.com>
2020-07-22 15:54:35 +1000

    Added logging to MQTT messages
    the mqqt python script now logs the data recieved on a topic into a folder for later review

--- Software/MQTTsystem.py
+++ Software/MQTTsystem.py
@@ -3,6 +3,12 @@ import numpy as np
 import datetime
 import paho.mqtt.client as mqtt
 
+fileName = 'officeTest.txt'
+
+def writeFile(filename, data):
+	file = open(filename, "a")
+	file.write(data)
+	file.close()
 
 def on_connect(client, userdata, flags, rc):
   print("Connected with result code "+str(rc))
@@ -13,13 +19,17 @@ def on_subscribe(client, userdata, mid, granted_qos):
 
 def on_message(client, userdata, msg):
     # print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
-    print(str(msg.payload))
+    currTime = datetime.datetime.now()
+    data = str(msg.payload, 'utf-8')
+    logString = str(currTime) + ", " + data + "\n"
+    writeFile(fileName, logString)
+    print(logString)
        
 
 client = mqtt.Client()
 client.on_subscribe = on_subscribe
 client.on_message = on_message
-client.connect("192.168.0.100", 1883)
+client.connect("192.168.1.4", 1883)
 client.subscribe("temp1", qos=1)
 
 client.loop_forever()
--- Software/visData.py
+++ Software/visData.py
@@ -3,14 +3,17 @@ import matplotlib.pyplot as plt
 import datetime
 
 data = pd.read_csv('hotWaterTest.txt', header = None)
+data[0] = pd.to_datetime(data[0], infer_datetime_format=True)
+data = data.set_index(0)
+data = data.resample('1T').mean()
 print(data)
 
 def visData(data):
-    plt.plot(data[0], data[1], label = 'Channel 1' )
-    plt.plot(data[0], data[2], label = 'Channel 2' )
-    plt.plot(data[0], data[3], label = 'Channel 3' )
-    plt.plot(data[0], data[4], label = 'Channel 4' )
-    plt.plot(data[0], data[5], label = 'Channel 5' )
+    plt.plot(data.index, data[1], label = 'Channel 1' )
+    plt.plot(data.index, data[2], label = 'Channel 2' )
+    plt.plot(data.index, data[3], label = 'Channel 3' )
+    plt.plot(data.index, data[4], label = 'Channel 4' )
+    plt.plot(data.index, data[5], label = 'Channel 5' )
     plt.legend()
     plt.title('Temperature Monitoring')
     plt.xlabel('Time')
