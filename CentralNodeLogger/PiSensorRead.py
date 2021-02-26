b4466cad73cb4676a64ef42eaa2f3211403d60ee
Andrew <andrew.collison.ac@gmail.com>
2021-02-26 10:31:12 +1000

    Integrated SQL
    Added sql database to central logging node. This will mean the data is both in the database and logged to a .txt file.

--- CentralNodeLogger/PiSensorRead.py
+++ CentralNodeLogger/PiSensorRead.py
@@ -5,11 +5,33 @@ import time
 import sys
 from threading import Thread
 import glob
+import sqlite3
 
 def writeFile(filename, data):
-	file = open(filename, "a")
-	file.write(data)
-	file.close()
+	try:
+		file = open(filename, "a")
+		file.write(data)
+		file.close()
+	except:
+		currTime = str(datetime.datetime.now())
+		err_str = currTime + ": Error writing to file: " + filename + ", " + data 
+		writeFile("ErrorLog.txt", err_str)
+		print(err_str)
+
+def writeDatabase(db_name, db_table, date_time, data_string, com_port):
+	try:
+		conn = sqlite3.connect(db_name)
+		c = conn.cursor()
+		sql_command = "INSERT INTO {db_table}(date_time, level_data, com_port) VALUES( '{date_time}', '{data_string}', '{port}' )"\
+			.format(db_table = db_table, date_time = date_time, data_string = data_string, port = com_port) 
+		print(sql_command)
+		c.execute(sql_command)
+		conn.commit()
+		conn.close()
+	except:
+		err_str = date_time + ": Error writing to database: " + db_name + ", " + db_table 
+		writeFile("ErrorLog.txt", err_str)
+		print(err_str)
 
 def serial_ports():
     """ Lists serial port names
@@ -43,14 +65,15 @@ def runA(port):
 	while True:
 		try:
 			serport = port
-			ser = Serial(str(serport))
+			ser = Serial(serport)
 			ser_bytes = ser.readline()
 			decoded_bytes = str(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
 			currTime = datetime.datetime.now()
-			results =  str(currTime) + ', ' + decoded_bytes + '\n'
+			results =  str(currTime) + ', ' + decoded_bytes + ', ' + port +'\n'
 			ser.close()
 			fileName = "Data_"+port+".txt"
 			writeFile(fileName, results)
+			writeDatabase('CentralDataBase.db', 'SenseData', str(currTime), decoded_bytes, port)
 			print(results)
 			time.sleep(1)
 		
@@ -64,14 +87,14 @@ def runA(port):
 if __name__ == "__main__":
 	
 	threads = []
-	# comList = ["COM6", "COM5", "COM7"]
 	comList = serial_ports()
 	print(comList)
-	# print(comList[0])
+
 	for i in range(len(comList)):
 		t = Thread(target = runA, args=[comList[i]])
 		t.setDaemon(True)
 		t.start()
 		threads.append(t)
+
 	while True:
 		pass
